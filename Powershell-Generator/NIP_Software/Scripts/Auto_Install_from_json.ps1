368e926c466f012d856ce3967f937539ee53d0b2
Thomas Coulson <122927373+TC-CoulsonLogic@users.noreply.github.com>
2023-08-09 20:46:18 +0100

    Update Auto_Install_from_json.ps1
    # Date: 2023/08/09
    #    Update: Added logic for multiple jsons in install directory

--- Powershell-Generator/NIP_Software/Scripts/Auto_Install_from_json.ps1
+++ Powershell-Generator/NIP_Software/Scripts/Auto_Install_from_json.ps1
@@ -5,9 +5,9 @@
 #
 #  Description:
 #  This script will auto run a json in NIP_software\auto
-# Date: 2023/08/09 
-#    Update: Added file checks for if multiple files match json name
 #
+# Date: 2023/08/09
+#    Update: Added logic for multiple jsons in install directory
 ########################################################################
 
 
@@ -18,59 +18,68 @@ $runningPS = Get-WmiObject Win32_Process -Filter "Name='powershell.exe' AND Comm
 $folder = 'C:\NIP_software'
 
 function Test-FileLock {
-    param ([parameter(Mandatory=$true)][string]$Path)
+  param ([parameter(Mandatory = $true)][string]$Path)
 
-$oFile = New-Object System.IO.FileInfo $Path
+  $oFile = New-Object System.IO.FileInfo $Path
 
-if ((Test-Path -Path $Path) -eq $false)
-{
-  return $false
-}
+  if ((Test-Path -Path $Path) -eq $false) {
+    return $false
+  }
 
-try
-{
+  try {
     $oStream = $oFile.Open([System.IO.FileMode]::Open, [System.IO.FileAccess]::ReadWrite, [System.IO.FileShare]::None)
-    if ($oStream)
-    {
+    if ($oStream) {
       $oStream.Close()
     }
     $false
-}
-catch
-{
-  # file is locked by a process.
-  return $true
-}
+  }
+  catch {
+    # file is locked by a process.
+    return $true
+  }
 }
 
 If ($runningps.Count -gt 1) { exit }
 
 #Wait for file
 do {
-   $filecheck = Test-Path "$folder\auto\*" -Include '*json'
+  $filecheck = Test-Path "$folder\auto\*" -Include '*json'
 
 }until ($filecheck -eq $True)
 
 $installjson = Get-ChildItem $folder\auto | Where-Object { $_.Name -Like '*.json' }
 
-$installjsonName = Get-ChildItem $folder\auto| Where-Object { $_.Name -Like '*.json' } | Select-Object -ExpandProperty Name
+if ($installjson.count -gt 1) {
+  foreach ($a in $installjson) {
+    $JsonValidityCheck = ((get-content $a.FullName) | ConvertFrom-Json ).VirtualizationSettings.DefaultDispositionLayer
+    if ($null -ne $JsonValidityCheck) {
+      $installjsonName = $a | Select-Object -ExpandProperty Name
+    }
 
-if ($installjsonName -notlike "*sccmauto.json"){
+  }
 
-do {
+}
+else {
+  $installjsonName = Get-ChildItem $folder\auto | Where-Object { $_.Name -Like '*.json' } | Select-Object -ExpandProperty Name
+}
+
+
+if ($installjsonName -notlike "*sccmauto.json") {
+
+  do {
     $filecheck = Test-Path "$folder\auto\*" -Include '*msi', '*.exe', '*.ps1', '*.bat', '*.cmd'
 
- }until ($filecheck -eq $True)
+  }until ($filecheck -eq $True)
 
-$installfiles = Get-ChildItem $folder\auto | Where-Object { $_.Name -Like ($installjson.basename + "*.msi") -or $_.Name -Like ($installjson.basename + "*.exe") -or $_.Name -Like ($installjson.basename + "*.ps1") -or $_.Name -Like ($installjson.basename + "*.bat") -or $_.Name -Like ($installjson.basename + "*.cmd") } | Select-Object -ExpandProperty Name
+  $installfiles = Get-ChildItem $folder\auto | Where-Object { $_.Name -Like ($installjson.basename + "*.msi") -or $_.Name -Like ($installjson.basename + "*.exe") -or $_.Name -Like ($installjson.basename + "*.ps1") -or $_.Name -Like ($installjson.basename + "*.bat") -or $_.Name -Like ($installjson.basename + "*.cmd") } | Select-Object -ExpandProperty Name
 
-foreach ($Installfile in $Installfiles){
-do {
-    $filecheck = Test-FileLock $installfile
-    if ($filecheck -eq $True){sleep 5}
- }until ($filecheck -eq $False)
- }
- }
+  foreach ($Installfile in $Installfiles) {
+    do {
+      $filecheck = Test-FileLock $installfile
+      if ($filecheck -eq $True) { sleep 5 }
+    }until ($filecheck -eq $False)
+  }
+}
 
 #Run studio prep
 powershell.exe -ep bypass -file $folder\scripts\CloudpagingStudio-prep.ps1 $true
